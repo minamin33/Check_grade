189b486523f0f7e7cb9865a94fb1de9c48ec5478
Sherina <137211921+minamin33@users.noreply.github.com>
2024-03-05 08:48:23 -0500

    Update main.py
    inserted datatype

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from grade_calculation import GradeCalculation
 
 if __name__ == "__main__":
+
+    try:
+        num_work = int(input("How many assignments?"))
+        student = input("Student name:")
+        grade = input("What is the grade per assignment?")
+
+        
+        score = float(input("Enter score:"))
+        print(computegrade(score))
     
-
-try:
-    work = input(print("How many assignments?"))
-    student = input(print("Student name:"))
-    grade = input(print("What is the grade per assignment?"))
-
-    #score = float(input("Enter score:"))
-    #print(computegrade(score))
-
-except:
-    print ("Incorrect input, please try again")
-    
+    except:
+        print ("Incorrect input, please try again")
+        
