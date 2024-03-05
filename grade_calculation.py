122ad6ec7ce0a7d93a2aea440f70bbcb330f4a4e
Sherina <137211921+minamin33@users.noreply.github.com>
2024-03-05 08:43:34 -0500

    Update grade_calculation.py
    add

--- grade_calculation.py
+++ grade_calculation.py
@@ -1,1 +1,15 @@
 
+def computegrade (score):
+    if(score >= 0 & score <= 1.0):
+        if(score >= 0.9):
+            return ("A")
+        elif(score >= 0.8):
+            return ("B")
+        elif(score >= 0.7):
+            return ("C")
+        elif (score >= 0.6):
+            return ("D")
+        else:
+            return("F")
+    else:
+        return ("Incorrect input, please try again")
