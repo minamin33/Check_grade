6a8d79820b4880c6d8c3da9e6ecf4641ae331002
Sherina <137211921+minamin33@users.noreply.github.com>
2024-03-07 12:52:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from grade_calculation import GradeCalculation
-from average_grade import
+from average_grade import AverageGrade
 
 if __name__ == "__main__":
 
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         student_grades = grade.split()
         for i in range (len(student_grades)):
             student_grades[i]= int(student_grades[i])
-        result = 
+        result = AverageGrade(student_grad
 
         
         score = float(input("Enter score:"))
