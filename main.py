58bfa8d774c0ec8a88c593d52b3a94dfc45db936
Sherina <137211921+minamin33@users.noreply.github.com>
2024-03-08 16:51:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from grade_calculation import GradeCalculation
-
+from average_grade import calculate_avg
 
 if __name__ == "__main__":
 
@@ -8,20 +8,18 @@ if __name__ == "__main__":
         num_students = int(input("Enter the number of students: ")
         for i in range(num_students):
         name = input("Enter the student name: ")
-        grades_str = float(input("Enter (name) assignments:")
-                       
-            
+        grades_str = float(input("Enter (name) assignments:")  
                 
         grades = grade_str.split()
         for i in range (len(grades)):
             grades[i]= float(grades[i])
         search = ("Enter the student name to search: ")
 
-        
-        
-
-        
-        
-    except:
-        print ("Incorrect input, please try again")
-        
+        if name in student_grade:
+            avg_grade = calculate_avg(student_grade(name))
+            final_grade = GradeCalculation(avg_grade)
+            print("The average grade for (name) is : (final_grade)")
+        else:
+            print("Student (name) is not found" )
+            print(" Would you like to insert them?")
+            
