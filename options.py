66ba2416b5e73a6cdd4b90f04c5d8e616fda0b80
Sherina <137211921+minamin33@users.noreply.github.com>
2024-03-10 10:48:21 -0400

    Update options.py

--- options.py
+++ options.py
@@ -1,1 +1,41 @@
+from average import average_grade
+class GradeManager:
+    def __init__(self):
+        self.student = {}
+    def enter_students(self):
+        try:
+            num_students = int(input("Enter the number of students: "))
+            for i in range(num_students):
+                student_id = input("Enter the student ID: ")
+                name = input("Enter the student name: ")
+                self.student[student_id] = {"name" : name, "grade": [] }
+            print("Students added successfully!")
+
+        except:
+            print("Invalid input, please add a number")
+
+    def enter_assignments(self):
+        try:
+            student_id = input("Enter the student ID to add assignments: ")
+            if student_id in self.student:
+                grades_str = input("Enter " + self.students[student_id]["name"]  + " assignments, space seperated ")
+                grades_list = [float(grade) for grade in grades_str.split()]
+                self.students[student_id]["grades"] = grades_list
+                print("Assignments added successfully!")
+        except:
+            print("Invalid input, please add grades")
+    def find_students_assignments(self):
+        name = input("Enter student name to find assignments: ")
+        if name in self.student:
+            print( name + "assignments: "+ self.student(name))
+        else:
+            print(name + "not found")
+    def find_students_average(self):
+        name = input("Enter student name get average score: ")
+        if name in self.student:
+            avg_score =  average_grade.calculate_avg(self.student(name))
+            print(name + " average score is " + avg_score)
+    def quit_program(self):
+        print("Exiting the program")
+        exit()
 
